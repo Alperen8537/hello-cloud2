5015e3793bd8433177c22148dbcda506f627f9e5
Alperen <alperenalbayrak26@gmail.com>
2026-03-18 13:35:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-from flask import flask, render_template_string, request 
-import os 
-import psycops2
+from flask import Flask, render_template_string, request
+import os
+import psycopg2
 
-app = Flask(_name_)
+app = Flask(__name__)
+DATABASE_URL = os.getenv("DATABASE_URL", "")
 
-DATABASE_URL = os.getenv("DATABASE_URL"," ")
-
-HTML = ***
-
+HTML = """
 <!doctype html>
 <html>
 <head>
   <title>Buluttan Selam</title>
-  <stytle>
-      body { font-family; Arial; text-align: center; padding: 50px; background: #eef2f3; }
-hi { color #333 }
-form { margin: 20px auto; }
-input { padding: 10px; font-size: 16px; }
-button { padding: 10px 15px;background: #4CAF50; color: white; border; none; border-radius: 6px; cursor:pointer; }
-ul { list-style: none; padding: 0; }
-li { background: white; margin: 5px auto; width: 200px; padding: 8px; border-radius: 5px; }
-</style>
+  <style>
+    body { font-family: Arial; text-align: center; padding: 50px; background: #eef2f3; }
+    h1 { color: #333; }
+    form { margin: 20px auto; }
+    input { padding: 10px; font-size: 16px; }
+    button { padding: 10px 15px; background: #4CAF50; color: white; border: none; border-radius: 6px; cursor: pointer; }
+    ul { list-style: none; padding: 0; }
+    li { background: white; margin: 5px auto; width: 200px; padding: 8px; border-radius: 5px; }
+  </style>
+</head>
