96d32be755ca552b2bfb2de8088901d457d3d0c0
Alperen <alperenalbayrak26@gmail.com>
2026-03-18 13:33:23 +0300

    app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import flask, render_template_string, request 
+import os 
+import psycops2
+
+app = Flask(_name_)
+
+DATABASE_URL = os.getenv("DATABASE_URL"," ")
+
+HTML = ***
+
+<!doctype html>
+<html>
+<head>
+  <title>Buluttan Selam</title>
+  <stytle>
+      body { font-family; Arial; text-align: center; padding: 50px; background: #eef2f3; }
+hi { color #333 }
+form { margin: 20px auto; }
+input { padding: 10px; font-size: 16px; }
+button { padding: 10px 15px;background: #4CAF50; color: white; border; none; border-radius: 6px; cursor:pointer; }
+ul { list-style: none; padding: 0; }
+li { background: white; margin: 5px auto; width: 200px; padding: 8px; border-radius: 5px; }
+</style>
